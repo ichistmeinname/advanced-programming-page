9d349168d8644f96bb5b851d53ad83132ce36e90
Sandra Dylus <sad@informatik.uni-kiel.de>
2018-10-23 13:58:52 +0200

    Modify deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ git checkout master
 cabal exec site clean
 cabal exec site build
 git checkout gh-pages
-cp -r public/index.html .
+cp -r public/*.html .
 cp -r public/css .
 git add *.html
 git add css
